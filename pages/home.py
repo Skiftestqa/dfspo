332897e3c79ecf7b32f8e24fd53b072a654bbcdc
Skiftestqa <Skifsunset@gmail.com>
2016-10-27 10:53:13 -0700

    updated _verify_page and locator

--- pages/home.py
+++ pages/home.py
@@ -3,11 +3,13 @@ from base import IncorrectPageException
 
 class HomePage(BasePage):
 
-    _landing_page_body_id = "homepage"
+    _home_page_body_id = "homepage"
 
     def __init__(self, driver):
         super(LandingPage, self).__init__(driver)
 
     def _verify_page(self):
         try:
-            self.wait_for_element_visibility(10, )
+            self.wait_for_element_visibility(10, "id", "homepage")
+        except:
+            raise IncorrectPageException

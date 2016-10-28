35b07c4225235e1d745b7be851f13c2ba386e20b
skiftestqa <skiftestqa@gmail.com>
2016-10-28 14:08:00 -0700

    added login page

--- pages/home.py
+++ pages/home.py
@@ -4,8 +4,9 @@ from join import JoinPage
 
 class HomePage(BasePage):
 
-    _body_id = "homepage"
+    _body_id = "#homepage"
     _join_button = "div.join-menu-block > a"
+    _login_button = ".global-header-container .login-menu-block > a"
 
     def __init__(self, driver):
         super(HomePage, self).__init__(driver)
@@ -19,5 +20,8 @@ class HomePage(BasePage):
     def join(self):
     	self.click(10, "css", _join_button)
     	return JoinPage(self.driver)
+
+    def login(self):
+    	self.click(10, "")	
 
 

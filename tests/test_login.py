058d13d2c8d13bc15aeebe3a6991a78bc4144636
skiftestqa <skiftestqa@gmail.com>
2016-11-01 15:37:37 -0700

    created test_login.py

---
+++ tests/test_login.py
@@ -0,0 +1,11 @@
+from dfspo.base_test_case import BaseTestCase
+from dfspo.Constants import DFS_Constants
+from dfspo.pages.login import LoginPage
+import unittest
+
+class LoginTest(BaseTestCase):
+
+	def setUp(self):
+		super(LoginTest,self).setUp()
+
+		

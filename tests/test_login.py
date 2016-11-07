6c3f9777a8490de97b83188685889c50774ea22a
skiftestqa <skiftestqa@gmail.com>
2016-11-07 08:16:35 -0800

    removed .pyc files and updated test_login to start test from home page to test login_as_valid_user

--- tests/test_login.py
+++ tests/test_login.py
@@ -1,14 +1,15 @@
 from base_test_case import BaseTestCase
 from Constants import DFS_Constants
-from pages.login import LoginPage
+from pages.home import HomePage
 
 
 class TestLogin(BaseTestCase):
     def setUp(self):
         super(TestLogin, self).setUp()
-        self.navigate_to_page(DFS_Constants['Base_URL'] + "p/login#login")
+        self.navigate_to_page(DFS_Constants['Base_URL'])
 
     def test_user_can_login_with_valid_credentials(self):
-        login_page_obj = LoginPage(self.driver)
+        home_page_obj = HomePage(self.driver)
+        login_page_obj = home_page_obj.click_login_button()
         login_page_obj.login_as_valid_user(DFS_Constants['Username'],
                                            DFS_Constants['Password'])

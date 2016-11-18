ffc99d124f9bab484a1e635f25db726a26fda9e1
skiftestqa <skiftestqa@gmail.com>
2016-11-17 19:09:59 -0800

    updated conftest to set Sauce jobs to pass or fail

--- tests/conftest.py
+++ tests/conftest.py
@@ -5,8 +5,6 @@ arguments using helper method pytest_addoption(parser).
 
 """
 
-
-
 import pytest
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
@@ -31,7 +29,7 @@ def pytest_addoption(parser):
                      help="base usrl for application under test")
     parser.addoption("--browser",
                      action="store",
-                     default="firefox",
+                     default="internet explorer",
                      help="the name of the browser you want to test with")
     parser.addoption("--host",
                      action="store",
@@ -45,6 +43,19 @@ def pytest_addoption(parser):
                      action="store",
                      default="Windows 7",
                      help="the operating system to run your tests on(saucelabs only)")
+
+
+@pytest.hookimpl(tryfirst=True, hookwrapper=True)
+def pytest_runtest_makereport(item, call):
+    """Helper method.
+
+    Used to grab the outcome of a test as it's running
+    and append the result to a node in the request object
+    that we're using in our fixture.
+    """
+    outcome = yield
+    result = outcome.get_result()
+    setattr(item, "result_" + result.when, result)
 
 
 @pytest.fixture
@@ -63,40 +74,50 @@ def driver(request):
 
     # saucelab test run setup
     if config.host == "saucelabs":
-        _desired_caps = {}
+        _desired_caps = {}  # Desired Capabilities object which is passed to Sauce Labs
         _desired_caps["browserName"] = config.browser
         _desired_caps["version"] = config.browserversion
         _desired_caps["platform"] = config.platform
+        _desired_caps["name"] = request.cls.__name__ + "." + request.function.__name__  # pass test name to SauceLabs
         _credentials = os.environ["SAUCE_USERNAME"] + ":" + os.environ["SAUCE_ACCESS_KEY"]
         _url = "http://" + _credentials + "@ondemand.saucelabs.com:80/wd/hub"
-        driver_ = webdriver.Remote(_url, _desired_caps)
+        driver_ = webdriver.Remote(_url, _desired_caps)  # connect to Sauce Labs using Selenium Remote
 
     # if command-line value set to localhost check browser value to determine which browser to launch locally
     if config.host == "localhost":
-        if config.browser == "firefox": # create webdriver instance of Firefox
+        if config.browser == "firefox":  # create webdriver instance of Firefox
             driver_ = webdriver.Firefox()
             driver_.maximize_window()
-        elif config.browser == "chrome": # create webdriver instance of Chrome
+        elif config.browser == "chrome":  # create webdriver instance of Chrome
             if platform.system() == "Darwin":  # locate chromedriver if test run on OS X
                 chromedriver = os.getcwd() + "/../vendor/chromedriver"
-            elif platform.system() == "Windows": # locate chromedriver if test run on Windows
+            elif platform.system() == "Windows":  # locate chromedriver if test run on Windows
                 chromedriver = os.getcwd() + "/../vendor/chromedriver.exe"
             options = Options()
-            options.add_argument("--start-maximized") # start chrome window maximized
+            options.add_argument("--start-maximized")  # start chrome window maximized
             driver_ = webdriver.Chrome(chromedriver, chrome_options=options)
-        elif config.browser == "ie32": # create webdriver instance of Internet Explorer
+        elif config.browser == "ie32":  # create webdriver instance of Internet Explorer
             iedriver = os.getcwd() + "/../vendor/IEDriverServer32.exe"
             driver_ = webdriver.Ie(iedriver)
             driver_.maximize_window()
         elif config.browser == "ie64":
-        	iedriver = os.getcwd() + "/../vendor/IEDriverServer64.exe"  
-        	driver_ = webdriver.Ie(iedriver)
-        	driver_.maximize_window()  
+            iedriver = os.getcwd() + "/../vendor/IEDriverServer64.exe"
+            driver_ = webdriver.Ie(iedriver)
+            driver_.maximize_window()
         else:
             raise Exception("This browser is not supported at the moment")
 
         def quit():
-            driver_.quit()
+            try:
+                if config.host == "saucelabs":
+                    if request.node.result_call.failed:
+                        driver_.execute_script("sauce:job-result=failed")  # communicate to Sauce what outcome was
+                        # output URL of the job to the console
+                        print "http://saucelabs.com/beta/tests/" + driver_.session_id
+                    elif request.node.result_call.passed:
+                        driver_.execute_script("sauce:job_result=passed")
+            finally:
+                driver_.quit()
 
         request.addfinalizer(quit)
         return driver_
